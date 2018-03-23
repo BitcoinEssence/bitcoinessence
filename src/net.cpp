0e295052b8aa6a7019652338f0c84bfe741d87ef
Masao Hidemitsu <shirakox@protonmail.ch>
2018-03-23 20:46:25 +0900

    BTE: Branding

--- src/net.cpp
+++ src/net.cpp
@@ -1479,7 +1479,7 @@ void ThreadMapPort()
             }
         }
 
-        std::string strDesc = "Litecoin " + FormatFullVersion();
+        std::string strDesc = "BitcoinEssence " + FormatFullVersion();
 
         try {
             while (true) {
